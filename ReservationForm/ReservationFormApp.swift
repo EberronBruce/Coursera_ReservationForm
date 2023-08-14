38dfc816fe43438c7774d1fae26991afba446d3d
Bruce <bruce@redravencomputing.com>
2023-08-14 01:22:06 -0500

    Initial Commit

---
+++ ReservationForm/ReservationFormApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReservationFormApp.swift
+//  ReservationForm
+//
+//  Created by Bruce Burgess on 8/14/23.
+//
+
+import SwiftUI
+
+@main
+struct ReservationFormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
