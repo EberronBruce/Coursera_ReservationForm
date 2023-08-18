d555a823d2e298088a0b30d4a9fb8e506f97bebd
Bruce <bruce@redravencomputing.com>
2023-08-17 23:32:00 -0500

    Added another view and experiment

---
+++ ReservationForm/MainView.swift
@@ -0,0 +1,37 @@
+//
+//  MainView.swift
+//  ReservationForm
+//
+//  Created by Bruce Burgess on 8/17/23.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        ZStack {
+            Color.green.opacity(0.5)
+                .ignoresSafeArea()
+            HStack(spacing: 8) {
+                Text("Demo")
+                    .scaledToFit()
+                    .frame(width: 100, height: 100, alignment: .center)
+                VStack(spacing: 10) {
+                   Text("Little Lemon")
+                       .font(.title)
+                   Text("Tomato Tortellini, Bottarga and Carbonara")
+                       .font(.title3)
+                       .multilineTextAlignment(.center)
+               }
+                .padding()
+            }
+            .padding()
+        }
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
